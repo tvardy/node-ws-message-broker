a910b2044444cbb1200c8f0c33e2e9ee0d4463fd
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2023-01-30 18:30:05 +0000

    remove all redundant express bloatware

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const http = require('http')
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
 const pocket = require('pocket.io')
 
-const app = express()
+const app = (_, res) => {
+  res.writeHead(200)
+  res.end('<pre>It works!</pre>')
+}
+
 const server = http.Server(app)
 const io = pocket(server)
 
 const allowedStrings = ['data', 'message']
-
-app.use(cors())
-app.use(helmet())
-
-app.get('/', (_, res) => {
-  res.send('<pre>It works!</pre>')
-})
 
 io.on('connection', (socket) => {
   allowedStrings.forEach((name) => {
